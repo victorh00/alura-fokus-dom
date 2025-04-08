fe6f2c0f269a02ced5d9c6fcc2f7da8264d01ca5
Victor Hugo <victorvh06@gmail.com>
2025-04-08 08:00:58 -0300

    meio aula 2

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-const hmtl = document.querySelector('html');
+const html = document.querySelector('html');
 const focoBtn = document.querySelector('.app__card-button--foco');
 const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
 
-focoBtn.addEventListener('click', () => {
-    hmtl.setAttribute('data-contexto', 'foco');
-});
+// focoBtn.addEventListener('click', () => {
+//     hmtl.setAttribute('data-contexto', 'foco');
+//     banner.setAttribute('src', '/imagens/foco.png');
+// });
 
-curtoBtn.addEventListener('click', () => {
-    hmtl.setAttribute('data-contexto', 'descanso-curto');
-})
+// curtoBtn.addEventListener('click', () => {
+//     hmtl.setAttribute('data-contexto', 'descanso-curto');
+//     banner.setAttribute('src', '/imagens/descanso-curto.png');
+// })
 
-longoBtn.addEventListener('click', () => {
-    hmtl.setAttribute('data-contexto', 'descanso-longo');
-})
+// longoBtn.addEventListener('click', () => {
+//    hmtl.setAttribute('data-contexto', 'descanso-longo');
+//    banner.setAttribute('src', '/imagens/descanso-longo.png');
+// })
+
+[focoBtn, curtoBtn, longoBtn].forEach((btn) => {btn.addEventListener('click', () => {
+    const contexto = btn.getAttribute('data-contexto');
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `/imagens/${contexto}.png`);
+})});
